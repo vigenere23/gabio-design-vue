5af540a761fca102a35e719dd11b4535118affd2
vigenere23 <lolgab1@hotmail.com>
2020-09-25 12:48:55 -0400

    fixed import

--- showcase/constants.ts
+++ showcase/constants.ts
@@ -1,4 +1,4 @@
-import { RelativeUrlResolver } from '../src/lib/utils/image'
+import { RelativeUrlResolver } from '../src/lib/utils/url'
 
 export const RELATIVE_URL_RESOLVER: RelativeUrlResolver = (url) =>
   require(`@showcase/assets/` + url)
