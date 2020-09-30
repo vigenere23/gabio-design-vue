3decf4b2fcac13ab829bf552ce952b5c2ed2d325
vigenere23 <lolgab1@hotmail.com>
2020-09-30 13:49:34 -0400

    webpack optimizations

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/gio-library-showcase/' : '/',
   configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()],
     resolve: {
       alias: {
         '@': path.join(__dirname, './src'),
         '@showcase': path.join(__dirname, './showcase')
+      }
+    },
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendors: {
+            test: /node_modules\/(?!antd\/).*/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
       }
     }
   },
